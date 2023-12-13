75568dcbdd932acc640cdafd6ed9d1a43487947d
VladAnohin <123352173+VladAnohin@users.noreply.github.com>
2023-12-13 13:56:10 +0700

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,184 @@
 import './App.css'
-import { useState } from 'react'
-import { MdDelete } from "react-icons/md";
-import { FaCheck } from "react-icons/fa";
+import { useState, useEffect } from 'react'
+import { MdDelete } from 'react-icons/md'
+import { FaCheck } from 'react-icons/fa'
 export const App = () => {
-   const [isCompleteScreen, setIsCompleteScreen] = useState(false);  
-   const [allTodos, setTodos] = useState([]);  
-   const [newTitle, setNewTitle] = useState('');
-   const [newDescription, setNewDescription] = useState('');
-  
-   const handleAddTodo = () => {
-      let newTodoItem = {
-        title: newTitle,
-        description: newDescription
-      }
-      let updatedTodoArr = [...allTodos];
-      updatedTodoArr.push(newTodoItem);
-      setTodos(updatedTodoArr);
-   }
-  
-   return (
-      <div className="App">
-       
-         <h1>My Todos</h1>
-        <div className="todo-wrapper">
-            <div className="todo-input">
-                <div className='todo-input-item'>
-                  <label>
-                    Title
-                  </label>
-                  <input type="text" value={newTitle} onChange={e => setNewTitle(e.target.value)} placeholder="What's the task title?"/>
+    const [isCompleteScreen, setIsCompleteScreen] = useState(false)
+    const [allTodos, setTodos] = useState([])
+    const [newTitle, setNewTitle] = useState('')
+    const [newDescription, setNewDescription] = useState('')
+    const [completedTodos, setCompletedTodos] = useState([])
+
+    const handleAddTodo = () => {
+        let newTodoItem = {
+            title: newTitle,
+            description: newDescription,
+        }
+        let updatedTodoArr = [...allTodos]
+        updatedTodoArr.push(newTodoItem)
+        setTodos(updatedTodoArr)
+        localStorage.setItem('todoList', JSON.stringify(updatedTodoArr)) // эта строка добавляет в локальное хранилище массив постов
+    }
+
+    const handleDeleteTodo = index => {
+        let reducedTodo = [...allTodos]
+        reducedTodo.splice(index, 1)
+
+        localStorage.setItem('todoList', JSON.stringify(reducedTodo))
+        setTodos(reducedTodo)
+    }
+    const handleCompletedTodo = index => {
+        let now = new Date()
+        let dd = now.getDate()
+        let mm = now.getMonth() + 1
+        let yyyy = now.getFullYear()
+        let h = now.getHours()
+        let m = now.getMinutes()
+        let s = now.getSeconds()
+        let completedOn =
+            dd + '-' + mm + '-' + yyyy + ' at ' + h + ':' + m + ':' + s
+
+        let filteredItem = {
+            ...allTodos[index],
+            completedOn: completedOn,
+        }
+        let updatedCompletedArr = [...completedTodos]
+        updatedCompletedArr.push(filteredItem)
+        setCompletedTodos(updatedCompletedArr)
+        handleDeleteTodo(index)
+        localStorage.setItem(
+            'completedTodos',
+            JSON.stringify(updatedCompletedArr)
+        )
+    }
+
+    const handleDeleteCompletedTodo = index => {
+        let reducedTodo = [...completedTodos]
+        reducedTodo.splice(index, 1)
+
+        localStorage.setItem('completedTodos', JSON.stringify(reducedTodo))
+        setCompletedTodos(reducedTodo)
+    }
+
+    useEffect(() => {
+        let savedTodo = JSON.parse(localStorage.getItem('todoList')) // этот useEffect используется при ререндере массива данных из локального хранилища
+        let savedCompletedTodo = JSON.parse(
+            localStorage.getItem('completedTodos')
+        )
+
+        if (savedTodo) setTodos(savedTodo)
+        if (savedCompletedTodo) {
+            setCompletedTodos(savedCompletedTodo)
+        }
+    }, [])
+    return (
+        <div className="App">
+            <h1>My Todos</h1>
+            <div className="todo-wrapper">
+                <div className="todo-input">
+                    <div className="todo-input-item">
+                        <label>Title</label>
+                        <input
+                            type="text"
+                            value={newTitle}
+                            onChange={e => setNewTitle(e.target.value)}
+                            placeholder="What's the task title?"
+                        />
+                    </div>
+                    <div className="todo-input-item">
+                        <label>Description</label>
+                        <input
+                            type="text"
+                            value={newDescription}
+                            onChange={e => setNewDescription(e.target.value)}
+                            placeholder="What's the task description?"
+                        />
+                    </div>
+                    <div className="todo-input-item">
+                        <button
+                            type="button"
+                            onClick={handleAddTodo}
+                            className="primaryBtn"
+                        >
+                            Add
+                        </button>
+                    </div>
                 </div>
-                 <div className='todo-input-item'>
-                  <label>
-                    Description
-                  </label>
-                  <input type="text" value={newDescription} onChange={e => setNewDescription(e.target.value)} placeholder="What's the task description?"/>
+                <div className="btn-area">
+                    <button
+                        className={`secondaryBtn ${
+                            isCompleteScreen === false && 'active'
+                        }`}
+                        onClick={() => setIsCompleteScreen(false)}
+                    >
+                        Todo
+                    </button>
+                    <button
+                        className={`secondaryBtn ${
+                            isCompleteScreen === true && 'active'
+                        }`}
+                        onClick={() => setIsCompleteScreen(true)}
+                    >
+                        Completed
+                    </button>
                 </div>
-                 <div className='todo-input-item'>
-                  <button type='button' onClick={handleAddTodo} className='primaryBtn'>
-                      Add
-                  </button>
+                <div className="todo-list">
+                    {isCompleteScreen === false &&
+                        allTodos.map((item, index) => {
+                            return (
+                                <div className="todo-list-item" key={index}>
+                                    <div>
+                                        <h3>{item.title}</h3>
+                                        <p>{item.description}</p>
+                                    </div>
+
+                                    <div className="icons">
+                                        <MdDelete
+                                            className="icon"
+                                            onClick={() =>
+                                                handleDeleteTodo(index)
+                                            }
+                                            title="Delete?"
+                                        />
+                                        <FaCheck
+                                            className="check-icon"
+                                            onClick={() =>
+                                                handleCompletedTodo(index)
+                                            }
+                                        />
+                                    </div>
+                                </div>
+                            )
+                        })}
+
+                    {isCompleteScreen === true &&
+                        completedTodos.map((item, index) => {
+                            return (
+                                <div className="todo-list-item" key={index}>
+                                    <div>
+                                        <h3>{item.title}</h3>
+                                        <p>{item.description}</p>
+                                        <p>
+                                            <small>
+                                                Completed on: {item.completedOn}
+                                            </small>
+                                        </p>
+                                    </div>
+
+                                    <div className="icons">
+                                        <MdDelete
+                                            className="icon"
+                                            onClick={() =>
+                                                handleDeleteCompletedTodo(index)
+                                            }
+                                            title="Delete?"
+                                        />
+                                    </div>
+                                </div>
+                            )
+                        })}
                 </div>
-            </div>
-            <div className="btn-area">
-              <button className={`secondaryBtn ${isCompleteScreen === false && 'active'}`} onClick={() => setIsCompleteScreen(false)}>Todo</button>
-              <button className={`secondaryBtn ${isCompleteScreen === true && 'active'}`}  onClick={() => setIsCompleteScreen(true)}>Completed</button>
-            </div>
-            <div className="todo-list">
-                {allTodos.map((item, index) => {
-                  return (
-              <div className='todo-list-item' key={index}>
-                <div>
-                  <h3>{item.title}</h3>
-                  <p>{item.description}</p>
-                </div>
-                
-                <div className='icons'>
-                    <MdDelete className='icon'/>
-                    <FaCheck className='check-icon'/>
-                </div>
-              </div>
-                  )
-                })}
             </div>
         </div>
-      </div>
     )
 }
-
-
